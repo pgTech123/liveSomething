10e9a990c3973e196a0f01b63b74263b0ee7369b
Laila Atrmouh <laila.atrmouh@gmail.com>
2013-11-16 10:12:22 -0500

    Géolocalisation !

---
+++ views/geolocation.js
@@ -0,0 +1,41 @@
+  
+	/**
+	*  Get a string address from coordinates
+	*/
+  function success(position) {
+	  geocoder = new google.maps.Geocoder();
+	  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+	  // Reverse Geocoding of the address
+	  geocoder.geocode({'latLng': latlng}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+	      if (results[1]) { 
+	      	// Récupérer uniquement le nom de la ville
+	      	var city_name;
+	      	console.log('kikoo ' + results[1].address_components.length);
+	      	for ( var i=0; i < results[1].address_components.length ; ++i)  {
+	      		console.log(results[1].address_components.length);
+	      		if ( results[1].address_components[i].types.indexOf('locality') > -1 ) {
+	      			city_name = results[1].address_components[i].long_name;
+	      			break;
+	      		}
+	      	}
+
+	        $('#location').html('Près de ' + city_name);
+	      } else {
+	        alert('No results found');
+	      }
+	    } else {
+	      alert('Geocoder failed due to: ' + status);
+	    }
+	  });
+
+}
+
+function error(msg) {
+  var s = document.querySelector('#status');
+  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+  s.className = 'fail';
+   
+}
+ 
