826300c498930730873e347650db90252c95c1d1
Laila Atrmouh <laila.atrmouh@gmail.com>
2013-11-17 12:25:38 -0500

    Evenements corrections

--- models/events.php
+++ models/events.php
@@ -3,18 +3,21 @@
 	function searchEvents($keyword, $lat, $long) {
 		if ( strpos($keyword, "aventure") !== FALSE  ) {
 			return array( "event" => array(
-						array('name'=> 'Chasse a cour', 'location' => 'Mont Bellevue (0.3 miles)', 'attendees' => 'Laila'),
-						array('name' => 'Randonnée', 'location' => 'Mont Orford (30 miles)', 'attendees' => 'Laila, Jean'),
+						array('name'=> 'Camping ', 'location' => 'Mont Bellevue (0.3 miles)', 'attendees' => 'Laila'),
+						array('name' => 'Randonnée', 'location' => 'Mont Orford (30 miles)', 'attendees' => 'Paul, Jean'),
 						array('name' => 'Chasse à l orignal', 'location' => 'Acton Vale (40 miles)', 'attendees' => 'Gilles')
 					));
 		}
-		else {
+		if ( strpos($keyword, "Magasiner") !== FALSE ) {
 			return array( "event" => array( 
 					array('name' => 'Magasinage pour Noel', 'location' => 'Centre Wellington', 'attendees' => 'Henri'),
-					array('name' => 'Soldes de vetements d hiver', 'location' => 'Sears - Carrefour de l Estrie', 'attendees' => 'Laila'),
-					array('name' => 'Soldes de tenues de soirée', 'location' => 'Simons - Carrefour de l Estrie', 'attendees' => 'Laila')
+					array('name' => 'Soldes de vetements d hiver', 'location' => 'Sears - Carrefour de l Estrie', 'attendees' => 'Alice Beauregard'),
+					array('name' => 'Soldes de tenues de soirée', 'location' => 'Simons - Carrefour de l Estrie', 'attendees' => 'Gilbert')
 					));
 		}
+		// Tableau vide
+		// return array( "event" => ""); --> Solution la plus propre, but time is running out to make it work!
+		return -1;
 
 	}
 
--- views/events.js
+++ views/events.js
@@ -1,18 +1,21 @@
 // Search for an event
 function searchEvent(keyword, lat, longi) {
 	$.get( "../controllers/searchEvent.php", { kwd: keyword, latitude: lat, longitude : longi })
-	  .done(function( data ) {
-	  	if ( data.size == 0 ) {
-	  		content = "Il n'y a pas d'événements correspondant à " + kwd + " ... Pourquoi pas le créer ?";
-	  	}
-	  	else { 
+	  .done(function( data ) { 
+	  		var json_array;
+	  		if ( data == -1 ) { 
+	  			json_array = {"event":[{"name":"Pas d'événement associé... Crées-en donc un !","location":"Où?","attendees":"Avec qui ?"}]};
+	  		}
+	  		else {
+				 json_array = $.parseJSON(data);
+	  		}
+	  		$('#resultats').empty();
 		    var template = $('#evtTempl').html(); 
 
-		    var json_array = $.parseJSON(data);
 			var content = Mustache.to_html(template, json_array); 	
-	  	}
-		$('#resultats').html( content);
-        $('#events').css("display", "block");
+	  	
+			$('#resultats').html( content );
+	        $('#events').css("display", "block");
 	  });
 
 
